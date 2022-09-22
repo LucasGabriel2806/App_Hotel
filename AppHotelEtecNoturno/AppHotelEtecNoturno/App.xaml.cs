bd7d10d37a7a79a9c963d8a27c034583ced3fbc2
Lucas Gabriel <pintolucas996@gmail.com>
2022-09-22 15:48:31 -0300

    Primeiro Commit

---
+++ AppHotelEtecNoturno/AppHotelEtecNoturno/App.xaml.cs
@@ -0,0 +1,62 @@
+﻿using AppHotelEtecNoturno.Model;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using System.Threading;
+using System.Globalization;
+
+namespace AppHotelEtecNoturno
+{
+    public partial class App : Application
+    {
+        public List<Suite> lista_suites = new List<Suite>
+        {
+            new Suite()
+            {
+               Nome = "Super Luxo",
+               DiariaAdulto = 110.0,
+               DiariaCrianca = 55.0
+            },
+            new Suite()
+            {
+               Nome = "Executiva",
+               DiariaAdulto = 90.0,
+               DiariaCrianca = 45.0
+            },
+            new Suite()
+            {
+               Nome = "Crise",
+               DiariaAdulto = 45.0,
+               DiariaCrianca = 20.0
+            }
+        };
+
+        public App()
+        {
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("pt-BR");
+
+            InitializeComponent();
+
+            //MainPage = new NavigationPage(new View.ContratacaoHospedagem());
+            
+            if(Properties.ContainsKey("usuario_logado"))
+                MainPage = new NavigationPage(new View.ContratacaoHospedagem());
+            else
+                MainPage = new NavigationPage(new View.Login());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ AppHotelEtecNoturno/AppHotelEtecNoturno/View/ContratacaoHospedagem.xaml.cs
@@ -0,0 +1,78 @@
+﻿using AppHotelEtecNoturno.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppHotelEtecNoturno.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ContratacaoHospedagem : ContentPage
+    {
+        App PropriedadesApp;
+
+        public ContratacaoHospedagem()
+        {
+            InitializeComponent();
+            NavigationPage.SetHasNavigationBar(this, false);
+
+            PropriedadesApp = (App) Application.Current;
+
+            lbl_usuario.Text = App.Current.Properties["usuario_logado"].ToString();
+
+            pck_suite.ItemsSource = PropriedadesApp.lista_suites;
+
+            dtpck_checkin.MinimumDate = DateTime.Now;
+            dtpck_checkin.MaximumDate = DateTime.Now.AddMonths(6);
+
+            dtpck_checkout.MinimumDate = DateTime.Now.AddDays(1);
+            dtpck_checkout.MaximumDate = DateTime.Now.AddMonths(6).AddDays(1);
+        }
+
+        private void dtpck_checkin_DateSelected(object sender, DateChangedEventArgs e)
+        {
+            DatePicker elemento = (DatePicker)sender;
+
+            dtpck_checkout.MinimumDate = elemento.Date.AddDays(1);
+            dtpck_checkout.MaximumDate = elemento.Date.AddMonths(6).AddDays(1);
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                Navigation.PushAsync(new HospedagemCalculada()
+                {
+                    BindingContext = new Hospedagem()
+                    {
+                        QntAdultos = Convert.ToInt32(lbl_qnt_adultos.Text),
+                        QntCriancas = Convert.ToInt32(lbl_qnt_criancas.Text),
+                        QuartoEscolhido = (Suite)pck_suite.SelectedItem,
+                        DataCheckIn = dtpck_checkin.Date,
+                        DataCheckOut = dtpck_checkout.Date
+                    }
+                });
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Ops", ex.Message, "OK");
+            }
+        }
+
+        private async void Button_Clicked_1(object sender, EventArgs e)
+        {
+            bool confime = await DisplayAlert("Tem Certeza?", 
+                                              "Desconectar sua conta ?",
+                                              "Sim", "Não");
+            if(confime)
+            {
+                App.Current.Properties.Remove("usuario_logado");
+                App.Current.MainPage = new Login();
+            }
+        }
+    }
+}
